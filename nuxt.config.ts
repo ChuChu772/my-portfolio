9ebdfe5cd0a83c5823f935af66c08084be5254c5
7chuchu2 <matsu310720@gmail.com>
2026-07-06 21:07:15 +0800

    w

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "cloudflare-pages",
-    cloudflare: {
-      deployConfig: true,
-      nodeCompat: true,
-    },
-  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
